21c923d5499a1d04c82ef3c822ac7fc7a16ff792
SeyedAb2 <tiggerwitht1378@gmail.com>
2021-07-13 16:32:54 +0430

    commiting html

---
+++ src/HowTo.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<style>
+    img{
+        max-width: 100%;
+    }
+</style>
+
+<div bp="grid">
+    <img bp="offset-5@md 4@md 12@sm" src="handwashing.gif" alt="How To Wash Your Hands.">
+</div>
